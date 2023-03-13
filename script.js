1f5accfd09816c49dd756dc08494a3e66f6116d2
nikhil-eashy <nikhileashy74@gmail.com>
2023-03-13 03:03:43 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+// var x=20;
+// {
+//     var x=10;
+//     console.log(x);
+// }
+// console.log(x);
+//  var sum=5+10;
+//  console.log(sum);
+//  var difference=10-5;
+//  console.log(difference);
+//  var multiple=5*5;
+//  console.log(multiple);
+//  var division=10/2;
+//  console.log(division);
+//  document.write("sum="+sum);
+//  document.write("multiple="+multiple);
+//  document.write("difference="+difference);
+//  document.write("division="+division)
+//  var fname="Tiya";
+//  var lname="Stephen";
+// document.write(`Fullname is ${fname} ${lname}`);
+
+
+
+// var student={
+//     name:"Tiya",
+//     age:10,
+//     place:"Koratty"
+// } 
+// console.log(student);
+// console.log(student.name);
+
+
+// var car={
+//     model:"BMW",
+//     color:"green",
+//     price:7500000,
+// }
+// console.log(car);
+
+// var person =[
+//     {
+//         name:"Tiya",
+//         age:10,
+//         place:"Koratty"
+//     },
+//     {
+//         name:"Ajas",
+//         age:"20",
+//         place:"Aluva"
+//     }
+// ]
+// console.log(person[1].name);
+
+// 
+// for(var i=1;i<=5;i++)
+// {
+//     console.log(i);
+// }
+// var fruits =["apple","orange","mango","grapes"];
+// for(let i=0;i<fruits.length;i++){
+//     console.log(fruits[i])
+// }
